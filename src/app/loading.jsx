f38fd2f09fe17442cb9cd6785770efdea567b0aa
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-05 00:00:27 +0600

    Added Loading Section

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+export default function loading() {
+    return (
+      <div className='flex justify-center mt-16'>
+        <img className='h-52' src='spinner.svg' alt='loading...' />
+      </div>
+    );
+}
